dbfd75361a532a38674e341cef8e12c94544f822
Murad <149615735+Abbasoffo@users.noreply.github.com>
2023-11-05 22:58:24 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,9 @@
+print("Hello world!")
+a=int(input("1 ci ədədi qeyd edin:"))
+b=int(input("2 ci ədədi daxil edin:"))
+if a>b:
+    print("{} {} böyükdür".format(a,b))
+elif a==b :
+    print(" {} {} Bərabərdi".format(a,b))
+else :
+    print("{} {} kiçikdir".format(a,b))
